a490880d8c3abbffc5e5c261934e425301d0886d
Kulpot <kulpotwahu@gmail.com>
2023-10-07 22:43:54 +0800

    ....

--- Our First Assembly Program/Mainer.cpp
+++ Our First Assembly Program/Mainer.cpp
@@ -2,6 +2,9 @@
 //ref link:https://www.youtube.com/watch?v=Yz1dl7KzsFs&list=PLRwVmtr-pp05c1HTBj1no6Fl6C6mlxYDG&index=4
 //
 
+// MyAssembly.asm - (customBuildTool)commandline - ml /c /Cx /cof "%(FullPath)"
+// MyAssembly.asm - (customBuildTool)output - %(Filename).obj
+
 extern "C" void doit();
 
 void main()
